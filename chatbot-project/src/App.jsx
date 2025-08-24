ec50a9aa4a26088d17d292270cf43986ed422a89
Mohamed-Inthisham <mohamed.inthisham999@gmail.com>
2025-08-24 21:09:28 +0530

    create proper react project and separate the files

---
+++ chatbot-project/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { ChatInput } from './components/ChatInput'
+import ChatMessages from './components/ChatMessages'
+import './App.css'
+
+
+function App(){
+  const [chatMessages, setChatMessages] = useState([{  // arraydestructuring for useState
+    message: 'hello from props',
+    sender: 'user',
+    id: 'id1'
+  },{
+      message: 'hi from props robot',
+      sender: 'robot',
+      id: 'id2'
+  }])
+
+  // const[chatMessages, setChatMessages] =array;  arraydestructuring
+  // const chatMessages = array[0];
+  // const setChatMessages = array[1];
+  return (
+    <div className = "app-container"> 
+      <ChatMessages 
+        chatMessages ={chatMessages}
+      />
+      <ChatInput 
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+
+  );
+}
+
+export default App
